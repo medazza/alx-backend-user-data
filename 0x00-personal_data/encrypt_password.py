6166bcfcae510fd78d3673a741cd66603735ae4e
medazza <midazza27@gmail.com>
2024-05-30 08:50:22 +0100

    task 5

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""  Encrypting passwords module """
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """function that expects one string argument name password 
+        and returns a salted"""
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """doc doc doc"""
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
