005cb9e01bc28762ba6db9b0d75290b8b64e331a
medazza <midazza27@gmail.com>
2024-06-06 23:28:57 +0100

    try again

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -46,7 +46,7 @@ def before_request_func():
                                             ]):
         return
     if auth.authorization_header(request) is None and \
-                    auth.session_cookie(request) is None:
+            auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
